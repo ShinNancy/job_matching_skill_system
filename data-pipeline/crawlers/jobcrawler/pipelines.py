f389e57d47868025e91bc45c97065426d831a2e0
tu.nguyen <tu.nguyen>
2026-06-30 13:10:31 +0700

    Tu - Update Fix docker connection

--- data-pipeline/crawlers/jobcrawler/pipelines.py
+++ data-pipeline/crawlers/jobcrawler/pipelines.py
@@ -157,7 +157,7 @@ class PostgresPipeline:
         self.conn = psycopg2.connect(
             dsn=os.getenv(
                 "DATABASE_URL",
-                "postgresql://postgres:postgres@localhost:5432/jobmatching"
+                "postgresql://postgres:postgres@localhost:5437/jobmatching"
             )
         )
         self.conn.autocommit = False
